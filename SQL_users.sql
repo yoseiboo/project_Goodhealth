02fca054b2b4d726a5aaeb97624323afdce4bbab
Rapeenan <86276071+yoseiboo@users.noreply.github.com>
2022-12-14 13:43:32 +0700

    Add files via upload

---
+++ SQL_users.sql
@@ -0,0 +1,8 @@
+Create database good_health ;
+Create table users (
+username varchar(255) NOT NULL ,
+password varchar(255) NOT NULL ,
+first_name varchar(255) NOT NULL ,
+last_name varchar(255) NOT NULL ,
+age int NOT NULL ,
+sex varchar(255) NOT NULL ) ;
