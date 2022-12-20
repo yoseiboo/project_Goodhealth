dca5407dabfb88dcc93160d1b2bddaef7901c3bc
Rapeenan <86276071+yoseiboo@users.noreply.github.com>
2022-12-20 09:35:25 +0700

    Delete SQL_users.sql

--- SQL_users.sql
+++
@@ -1,8 +0,0 @@
-Create database good_health ;
-Create table users (
-username varchar(255) NOT NULL ,
-password varchar(255) NOT NULL ,
-first_name varchar(255) NOT NULL ,
-last_name varchar(255) NOT NULL ,
-age int NOT NULL ,
-sex varchar(255) NOT NULL ) ;
