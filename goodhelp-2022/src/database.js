34c362930cc5a3041aff046785bb9be878c31940
Rapeenan <86276071+yoseiboo@users.noreply.github.com>
2022-12-27 13:57:54 +0700

    Delete goodhelp-2022 directory

--- goodhelp-2022/src/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const dbConnection = mysql.createPool({
-    host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root', // MYSQL USERNAME
-    password : 'root', // MYSQL PASSWORD
-    database : 'nodejs_login' // MYSQL DB NAME
-}).promise();
-module.exports = dbConnection;
